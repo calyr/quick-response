074270597f33e45a521c50a3f7f65244cea65a7f
calyr <calyr.software@gmail.com>
2016-12-27 10:37:25 -0400

    Initial Commit

---
+++ QuickResponse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickResponse
+//
+//  Created by Roberto Carlos Callisaya Mamani on 12/27/16.
+//  Copyright © 2016 Roberto Carlos Callisaya Mamani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
